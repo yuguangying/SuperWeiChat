fcaad4ca6668cd92f4bac344653dfc3b208ca70a
yuguangying <1847266416@qq.com>
2016-11-01 14:44:35 +0800

    闪屏，欢迎，登录，注册页面之间跳转，appkey的修改

--- ChatDemoUI3.0/src/cn/ucai/superwechat/ui/WelcomeActivity.java
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/ui/WelcomeActivity.java
@@ -1,15 +1,33 @@
 package cn.ucai.superwechat.ui;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import cn.ucai.superwechat.R;
 
-public class WelcomeActivity extends AppCompatActivity {
-
+public class WelcomeActivity extends BaseActivity implements View.OnClickListener{
+Button login,register;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        login = (Button) findViewById(R.id.welcome_login);
+        register = (Button) findViewById(R.id.welcome_register);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.welcome_login:
+                startActivity(new Intent(WelcomeActivity.this, LoginActivity.class));
+                finish();
+                break;
+            case R.id.welcome_register:
+                startActivity(new Intent(WelcomeActivity.this, RegisterActivity.class));
+                finish();
+                break;
+        }
     }
 }
